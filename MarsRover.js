eeb4a55feed0d9c039d44032e2db2de424e6d1c9
deepanar <deepanar@admins-MacBook-Pro-2.local>
2018-06-12 22:20:31 +0530

    added function to move mars rover

--- MarsRover.js
+++ MarsRover.js
@@ -1,7 +1,56 @@
-function MarsRover() {
-}
+function MarsRover(plateau_x, plateau_y, position_x, position_y, direction, control_sequence) {
+  this.plateau_x = plateau_x;
+  this.plateau_y = plateau_y;
+  this.position_x = position_x;
+  this.position_y = position_y;
+  this.direction = direction;
+  this.control_sequence = control_sequence;
 
-function initialiseMatrix(x, y) {
-  matrix = Array(x).fill(Array(y));
-  return matrix;
+  const directions = ['N', 'E', 'S', 'W']
+
+  function move() {
+    for(control_char of this.control_sequence) {
+      if(control_char === 'M') {
+        switch(this.direction) {
+          case 'N' :
+            this.plateau_y++;
+          case 'S' :
+            this.plateau_y--;
+          case 'E' :
+            this.plateau_x++;
+          case 'W' :
+            this.plateau_x--;
+        }
+      }
+      else {
+        this.direction = computeNewDirection(control_char);
+      }
+
+    }
+  }
+
+  function computeNewDirection(moveDirection) {
+    indexOfCurrentDirection = directions.findIndex(moveDirection);
+    switch(moveDirection) {
+      case 'L' :
+        if(indexOfCurrentDirection === 0) {
+          return directions[3];
+        }
+        else {
+          return directions[indexOfCurrentDirection - 1];
+        }
+      case 'R' :
+        if(indexOfCurrentDirection === 3) {
+          return directions[0];
+        }
+        else {
+          return directions[indexOfCurrentDirection + 1];
+        }
+    }
+  }
+
+  function checkPlateauBoundary(x, y) {
+    return console.error();
+  }
+
 }
