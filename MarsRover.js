6a8c9d60d58f4eef2fa7a14bd2f35ed381584f4a
deepanar <deepanar@admins-MacBook-Pro-2.local>
2018-06-13 00:14:35 +0530

    fix move function

--- MarsRover.js
+++ MarsRover.js
@@ -1,63 +1,70 @@
-function MarsRover(plateau_x, plateau_y, position_x, position_y, direction, control_sequence) {
+var MarsRover = function (plateau_x, plateau_y, position_x, position_y, direction, control_sequence) {
   this.plateau_x = plateau_x;
   this.plateau_y = plateau_y;
   this.position_x = position_x;
   this.position_y = position_y;
   this.direction = direction;
   this.control_sequence = control_sequence;
+  this.directions = ['N', 'E', 'S', 'W']
+}
 
-  const directions = ['N', 'E', 'S', 'W']
-
-  function move() {
-    for(control_char of this.control_sequence) {
-      if(control_char === 'M') {
-        try {
+MarsRover.prototype.move = function() {
+  for(control_char of this.control_sequence) {
+    console.log(control_char)
+    if(control_char === 'M') {
+      try {
         switch(this.direction) {
           case 'N' :
-          this.plateau_y++;
+          this.position_y += 1;
+          break;
           case 'S' :
-          this.plateau_y--;
+          this.position_y -=1;
+          break;
           case 'E' :
-          this.plateau_x++;
+          this.position_x +=1;
+          break;
           case 'W' :
-          this.plateau_x--;
+          this.position_x -=1;
+          break;
         }
-        checkPlateauBoundary(this.plateau_x, this.plateau_y)
+        this.checkPlateauBoundary(this.plateau_x, this.plateau_y)
       }
       catch(e) {
-        alert(e.message());
+        console.log(e.message);
       };
     }
     else {
-      this.direction = computeNewDirection(control_char);
+      this.direction = this.computeNewDirection(control_char);
     }
   }
 }
 
-  function computeNewDirection(moveDirection) {
-    indexOfCurrentDirection = directions.findIndex(moveDirection);
-    switch(moveDirection) {
-      case 'L' :
-        if(indexOfCurrentDirection === 0) {
-          return directions[3];
-        }
-        else {
-          return directions[indexOfCurrentDirection - 1];
-        }
-      case 'R' :
-        if(indexOfCurrentDirection === 3) {
-          return directions[0];
-        }
-        else {
-          return directions[indexOfCurrentDirection + 1];
-        }
-    }
+MarsRover.prototype.computeNewDirection = function(moveDirection) {
+  var indexOfCurrentDirection = this.directions.indexOf(this.direction);
+  switch(moveDirection) {
+    case 'L' :
+      if(indexOfCurrentDirection === 0) {
+        return this.directions[3];
+      }
+      else {
+        return this.directions[indexOfCurrentDirection - 1];
+      }
+    case 'R' :
+      if(indexOfCurrentDirection === 3) {
+        return this.directions[0];
+      }
+      else {
+        return this.directions[indexOfCurrentDirection + 1];
+      }
   }
-
-  function checkPlateauBoundary(x, y) {
-    throw "Cannot move further";
-  }
-
 }
 
-MarsRover(5, 5, 1, 2, 'N', 'LMLMLMLMM');
+MarsRover.prototype.returnPosition = function () {
+    return this.position_x + " " + this.position_y + " " + this.direction;
+};
+
+MarsRover.prototype.checkPlateauBoundary = function(x, y) {
+  if(this.position_x > this.plateau_x || this.position_x < 0 || this.position_y > this.plateau_y || this.plateau_y < 0) {
+    throw new Error("Cannot move further");
+  }
+}
